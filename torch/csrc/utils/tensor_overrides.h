3c7fa9e100aabf1047d0a357ebe7da16afb1e774
Nathan Goldbaum <nathan12343@gmail.com>
2020-01-06 16:17:58 -0600

    moving more implementation details that don't need to be exposed in the header to tensor_overrides.cpp

--- torch/csrc/utils/tensor_overrides.h
+++ torch/csrc/utils/tensor_overrides.h
@@ -31,96 +31,7 @@ namespace torch {
  *
  */
 
-static py::object PyObject_FastGetAttrString(PyObject *obj, char *name)
-{
-    PyTypeObject *tp = Py_TYPE(obj);
-    PyObject *res = (PyObject *)NULL;
-
-    /* Attribute referenced by (char *)name */
-    if (tp->tp_getattr != NULL) {
-        res = (*tp->tp_getattr)(obj, name);
-        if (res == NULL) {
-          PyErr_Clear();
-        }
-    }
-    /* Attribute referenced by (PyObject *)name */
-    else if (tp->tp_getattro != NULL) {
-        PyObject *w = THPUtils_internString(name);
-        if (w == NULL) {
-          return py::object();
-        }
-        res = (*tp->tp_getattro)(obj, w);
-        Py_DECREF(w);
-        if (res == NULL) {
-            PyErr_Clear();
-        }
-    }
-    return py::reinterpret_steal<py::object>(res);
-}
-
-// Makes sure that we don't check for __torch_function__ on basic Python types
-static bool _is_basic_python_type(PyTypeObject *tp)
-{
-  return (
-    /* Basic number types */
-    tp == &PyBool_Type ||
-
-    tp == &PyLong_Type ||
-    tp == &PyFloat_Type ||
-    tp == &PyComplex_Type ||
-
-    /* Basic sequence types */
-    tp == &PyList_Type ||
-    tp == &PyTuple_Type ||
-    tp == &PyDict_Type ||
-    tp == &PySet_Type ||
-    tp == &PyFrozenSet_Type ||
-    tp == &PyUnicode_Type ||
-    tp == &PyBytes_Type ||
-
-#if PY_MAJOR_VERSION == 2
-    tp == &PyString_Type ||
-#endif
-
-    /* other builtins */
-    tp == &PySlice_Type ||
-    tp == Py_TYPE(Py_None) ||
-    tp == Py_TYPE(Py_Ellipsis) ||
-    tp == Py_TYPE(Py_NotImplemented) ||
-
-    PyModule_Check(tp) ||
-    /* sentinel to swallow trailing || */
-    false
-  );
-}
-
-/*
- * Lookup a special method, following the python approach of looking up
- * on the type object, rather than on the instance itself.
- *
- * Assumes that the special method is a torch-specific one, so does not
- * look at builtin types, nor does it look at a base Tensor.
- *
- * If no special method is found, return NULL, otherwise returns a new
- * reference to the function object
- *
- * In future, could be made more like _Py_LookupSpecial
- */
-
-static py::object PyTorch_LookupSpecial(PyObject *obj, char* name)
-{
-  PyTypeObject *tp = Py_TYPE(obj);
-  if (THPVariable_CheckExact(obj)) {
-      return py::object();
-  }
-  if (_is_basic_python_type(tp)) {
-    return py::object();
-  }
-  if(PyObject_HasAttrString(obj, name) == 0){
-    return py::object();
-  }
-  return PyObject_FastGetAttrString((PyObject *)tp, name);
-}
+static py::object PyObject_FastGetAttrString(PyObject *obj, char *name);
 
 /*
  * Checks if obj has a __torch_function__ implementation
@@ -129,14 +40,7 @@ static py::object PyTorch_LookupSpecial(PyObject *obj, char* name)
  *
  */
 
-static auto check_has_torch_function(PyObject* obj) -> bool
-{
-  py::object method = PyTorch_LookupSpecial(obj, "__torch_function__");
-  if(method.ptr() != nullptr){
-    return true;
-  }
-  return false;
-}
+auto check_has_torch_function(PyObject* obj) -> bool;
 
 /*
  *  obj has a __torch_function__ implementation and may either be a
@@ -175,5 +79,10 @@ static auto check_has_torch_function(PyObject* obj) -> bool
  */
 
 void append_overloaded_arg(std::vector<py::handle> &overloaded_args, PyObject* obj);
-  
+
+namespace python {
+
+void _implement_torch_function(py::function implementation, py::function public_api, py::iterable relevant_args, py::tuple args, py::dict kwargs);
+
+} // namespace python
 } // namespace torch
